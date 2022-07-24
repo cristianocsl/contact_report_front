ccb55724c5092695da572a757108e483bcb74083
cristianocsl <cslcristiano@gmail.com>
2022-07-24 15:26:04 -0300

    feat: adiciona método post

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,28 +1,57 @@
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import React, { useEffect } from 'react';
 
 import './styles.css';
 
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  phone: '',
+  message: '',
+  attachedFile: {},
+}
 const FileForm = () => {
+  const [formState, setFormState] = useState(INITIAL_STATE);
 
-  useEffect(() => {
-    async function fetchData() {
-      const customer = JSON.parse(localStorage.getItem('user'));
-      if (customer) {
-        const { data } = await axios.get('http://localhost:3001/login/token', {
-          headers: { authorization: customer.token },
-        });
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newState = Object.assign({}, formState);
+    const formData = new FormData();
+    formData.append('attachedFile', newState.attachedFile);
+    formData.append('name', newState.name);
+    formData.append('email', newState.email);
+    formData.append('phone', newState.phone);
+    formData.append('message', newState.message);
+    
+    try {
+      const { data } = await axios.post('http://localhost:3001/recordContact', `${formData}`, {
+        headers: { 'Content-Type': 'multipart/form-data; application/json; charset=utf-8' },
+      });
+      console.log(data);
+
+      return data
+    } catch (err) {
+      console.log(err);
+      return err;
     }
-    fetchData();
-  }, []);
+  };
 
-  const postMethod = async () => {};
-  const getMethod = async () => {};
+  const handleFile = (e) => {
+    const copyState = Object.assign({}, formState);
+    setFormState({ ...copyState, attachedFile: e.target.files[0] });
+  };
+  
+  const handleChange = (e) => {
+    console.log(formState);
+    const { name, value } = e.target;
+    const copyState = Object.assign({}, formState);
+    setFormState({ ...copyState, [name]: value });
+  };
 
 
   return (
-    <form className="container-form">
+
+    <form className="container-form" onSubmit={ handleSubmit }>
       <p>Envie seus dados</p>
       <div className="content-form">
       <p>Nome</p>
@@ -30,6 +59,7 @@ const FileForm = () => {
           name="name"
           type="text"
           className="input-fields"
+          onChange={ handleChange }
           placeholder="digite o seu nome"
         />
         <p>Email</p>
@@ -37,6 +67,7 @@ const FileForm = () => {
           name="email"
           type="email"
           className="input-fields"
+          onChange={ handleChange }
           placeholder="email@email.com"
           />
         <p>Número de telefone</p>
@@ -44,23 +75,26 @@ const FileForm = () => {
           name="phone"
           type="text"
           className="input-fields"
+          onChange={ handleChange }
           placeholder="00 00000-0000"
         />
         <p>Mensagem</p>
         <textarea
           name="message"
           type="textarea"
+          onChange={ handleChange }
           className="input-fields"
         />
         <p>Anexar arquivo</p>
         <input
-          name="file"
+          name="attachedFile"
           type="file"
+          onChange={ handleFile }
           className="input-fields"
         />
         <button
           name="enviar"
-          type="button"
+          type="submit"
           className="btn-form"
         >
           ENVIAR
